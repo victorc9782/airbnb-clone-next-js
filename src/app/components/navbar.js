9969671855ca47f226f5c9f8e34e49b366ce6cb7
C.Victor <victorchu2002@gmail.com>
2024-02-12 23:33:39 +0000

    Signup function

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,15 +1,41 @@
 'use client';
+import React, { useState } from 'react';
+import SignupModal from "./modal/signUpModal";
 
 const NavBar = () => {
+    const [showSignupModal, setShowSignupModal] = useState(false);
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+
+    const openSignupModal = () => {
+        setShowSignupModal(true);
+    };
+    
+    const closeSignupModal = () => {
+        setShowSignupModal(false);
+    };
+    const handleSignupSubmit = (formData) => {
+        // Handle the form submission here
+        setUserName(formData.name);
+        setUserEmail(formData.email);
+        setUserPassword(formData.password);
+        closeSignupModal(); // Close the modal after form submission
+    };
     return (
         <nav class="navbar">
             <div class="logo">
                 <img src="/airbnb.png" alt="Airbnb Logo"/>
             </div>
+            {userName ? (
+                <span className="user-name">{userName}</span>
+                ) : (
             <div class="nav-buttons">
                 <button class="login-button">Login</button>
-                <button class="signup-button">Sign Up</button>
+                <button class="signup-button" onClick={openSignupModal}>Sign Up</button>
             </div>
+                )}
+            {showSignupModal && <SignupModal onClose={closeSignupModal} onSubmit={handleSignupSubmit}/>}
         </nav>
     )
         
